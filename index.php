c6a380fd5894ace1dbe68c208106dbce54e1a537
Charles RIBEMONT BARBARAN <charles@mac.local>
2021-01-14 17:36:04 +0100

    Upodate

--- index.php
+++ index.php
@@ -3,13 +3,26 @@ require 'vendor/autoload.php';
 
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
+use Dompdf\Options;
 
 // instantiate and use the dompdf class
-$dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+$html = file_get_contents(__DIR__ .  "/certificat_de_realisation.html");
+$html = str_replace("{folder}", "https://ns305834.ip-37-187-23.eu/bvf/dompdfBVF", $html);
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$options = new Options();
+$options->set('isRemoteEnabled', TRUE);
+$dompdf = new Dompdf($options);
+
+$context = stream_context_create([ 
+	'ssl' => [ 
+		'verify_peer' => FALSE, 
+		'verify_peer_name' => FALSE,
+		'allow_self_signed'=> TRUE 
+	] 
+]);
+$dompdf->setHttpContext($context);
+
+$dompdf->loadHtml($html);
 
 // Render the HTML as PDF
 $dompdf->render();
